eccb6a8dd293bb0af72a4e21a305976456408401
aqFalco <98854965+aqFalco@users.noreply.github.com>
2022-02-21 16:53:46 +0000

    Add files via upload

--- ScriptLinux.py
+++ ScriptLinux.py
@@ -62,7 +62,7 @@ s("ip link set enp0s3 up")
 s("ip link set enp0s8 down")
 s("apt update && apt upgrade -y")
 
-while (!os.path.isdir('/etc/dhcp/dhcpd') and !os.path.isdir('/etc/bind') and !os.path.isfile('/etc/asterisk')):
+while (os.path.isdir('/etc/dhcp/dhcpd') == False and os.path.isdir('/etc/bind') == False and os.path.isfile('/etc/asterisk') == False):
     for i in ["isc-dhcp-server", "bind9", "apache2", "asterisk", "default-jre", "mysql-server"]: #services to install
         s(apti + i)
 
